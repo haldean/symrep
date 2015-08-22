2b1c08e50b1fe382cd94ce23fadfb89f793f5a0f
Will Haldean Brown <will.h.brown@gmail.com>
2015-08-22 12:42:44 -0700

    simple nodes and node hierarchy visualization

---
+++ symrep.py
@@ -0,0 +1,47 @@
+import math
+
+def const(val):
+    return Node("const {}".format(val), lambda _: val, [])
+
+def sum(n1, n2):
+    return Node("add", lambda t: n1(t) + n2(t), [n1, n2])
+
+def product(n1, n2):
+    return Node("product", lambda t: n1(t) * n2(t), [n1, n2])
+
+def sine(freq):
+    return Node(
+        "sin", lambda t: math.sin(t * freq(t) / (2 * math.pi)), [freq])
+
+class Node(object):
+    _next_id = 0
+
+    def __init__(self, name, func, deps):
+        self.id = Node._next_id
+        Node._next_id += 1
+
+        self.name = name
+        self.func = func
+        self.deps = deps
+
+    def __call__(self, t):
+        return self.func(t)
+
+def collect_nodes(root):
+    return set([root]).union(
+        reduce(set.union, map(collect_nodes, root.deps), set()))
+
+def collect_edges(root):
+    edges = [(root.id, dep.id) for dep in root.deps]
+    for dep in root.deps:
+        edges.extend(collect_edges(dep))
+    return edges
+
+def to_dot(root, stream, name="symrep"):
+    stream.write("digraph {name} {{\n".format(name=name))
+    for node in collect_nodes(root):
+        stream.write("{id} [label=\"{name}\"];\n".format(
+            id=node.id, name=node.name))
+    for n1, n2 in collect_edges(root):
+        stream.write("{n2} -> {n1};\n".format(n1=n1, n2=n2))
+    stream.write("}\n")
---
+++ test_symrep.py
@@ -0,0 +1,65 @@
+import symrep
+import unittest
+
+
+class SymrepTest(unittest.TestCase):
+    def test_const(self):
+        n = symrep.const(4)
+        self.assertEqual(n(0), 4)
+        self.assertEqual(n(1), 4)
+        self.assertEqual(n(2), 4)
+
+    def test_sum(self):
+        n1 = symrep.const(1)
+        n2 = symrep.const(2)
+
+        n = symrep.sum(n1, n2)
+        self.assertEqual(n(0), 3)
+
+        n = symrep.sum(n1, n1)
+        self.assertEqual(n(0), 2)
+
+    def test_product(self):
+        n = symrep.product(symrep.const(1), symrep.const(-3))
+        self.assertEqual(n(0), -3)
+
+        n1 = symrep.const(-4)
+        n = symrep.product(symrep.const(-4), symrep.const(.5))
+        self.assertEqual(n(0), -2)
+
+    def test_sine(self):
+        n = symrep.sine(symrep.const(1))
+        self.assertEqual(n(0), 0)
+        self.assertEqual(n(0.25), 1)
+        self.assertEqual(n(0.5), 0)
+        self.assertEqual(n(0.75), -1)
+        self.assertEqual(n(1), 0)
+
+    def test_collect_nodes(self):
+        n1 = symrep.const(1)
+        n2 = symrep.sine(n1)
+        n3 = symrep.sum(n1, n2)
+        n4 = symrep.const(-2)
+        n5 = symrep.product(n3, n4)
+        nodes = symrep.collect_nodes(n5)
+        self.assertEqual(len(nodes), 5)
+        self.assertSetEqual(
+            set(n.id for n in nodes),
+            set((n1.id, n2.id, n3.id, n4.id, n5.id)))
+
+    def test_dot(self):
+        n = symrep.product(
+            symrep.sine(symrep.const(2)),
+            symrep.product(
+                symrep.sine(symrep.const(3)),
+                symrep.sum(
+                    symrep.const(1),
+                    symrep.const(1),
+                )
+            )
+        )
+        with open("test.dot", "w") as f:
+            symrep.to_dot(n, f, name="test_dot")
+
+if __name__ == "__main__":
+    unittest.main()
