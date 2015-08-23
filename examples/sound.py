2785f29324f801a0ed64befb31e2dfce33c2a41e
Will Haldean Brown <will.h.brown@gmail.com>
2015-08-22 18:38:05 -0700

    that's a nice sound.

--- examples/sound.py
+++ examples/sound.py
@@ -2,19 +2,23 @@ import symrep
 import sys
 
 n1 = symrep.product(
-    symrep.const(0.5),
+    symrep.const(0.3),
     symrep.product(
         symrep.audio.sine(symrep.const(440)),
         symrep.audio.sine(symrep.const(0.5)),
     )
 )
 n2 = symrep.product(
-    symrep.const(0.5),
+    symrep.const(0.3),
     symrep.product(
         symrep.audio.sine(symrep.const(360)),
         symrep.audio.sine(symrep.const(0.4)),
     )
 )
-n = symrep.sum(n1, n2)
+n3 = symrep.product(
+    symrep.const(0.3),
+    symrep.audio.sine(symrep.const(500)),
+)
+n = symrep.sum(n1, n2, n3)
 
 symrep.audio.to_wav(n, 44100, 10, sys.stdout)
